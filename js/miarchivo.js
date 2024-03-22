11bdc9c20ada5d2ac3d0266480f00ef45cfef09c
Brian Switach <brianswitach@Air-de-Brian.fibertel.com.ar>
2024-03-22 16:33:24 -0300

    commit reentrega

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,28 +1,29 @@
 
-const precios = [10, 20, 30];
-
+const productos = [
+  { nombre: "Producto 1", precio: 10 },
+  { nombre: "Producto 2", precio: 20 },
+  { nombre: "Producto 3", precio: 30 }
+];
 
 const descuentouno = 50;
-const descuentodos = 0.1; 
+const descuentodos = 0.1;
 
 function calculateTotalCost() {
   
-  let cantidades = precios.map((price, index) => {
-    let cantidad = parseInt(prompt(`Ingrese la cantidad para el Producto ${index + 1} ($${price}):`)) || 0;
-    return cantidad;
+  let cantidades = productos.map((producto) => {
+    let cantidad = parseInt(prompt(`Ingrese la cantidad para ${producto.nombre} ($${producto.precio}):`)) || 0;
+    return { ...producto, cantidad };
   });
 
+  
+  let total = cantidades.reduce((acc, { cantidad, precio }) => acc + (cantidad * precio), 0);
 
-  let total = cantidades.reduce((acc, cantidad, index) => acc + (quantity * precios[index]), 0);
-
- 
+  
   if (total > descuentouno) {
     total -= total * descuentodos;
   }
 
-  
   alert(`Costo Total: $${total.toFixed(2)}`);
 }
-
 
 calculateTotalCost();
